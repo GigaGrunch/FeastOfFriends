9ca6b262f310125eec1210e018f9c902182e5ce0
Andreas Pritschet <andreas@pritschet.eu>
2016-01-30 00:23:33 +0100

    added spotables

---
+++ Assets/Scripts/Spotables/Requirement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Requirement : Spotable
+{
+    enum Type
+    {
+        strentgh, speed
+    }
+
+    Type type;
+    int value;
+
+    Type getType()
+    {
+        return type;
+    }
+
+    int getValue()
+    {
+        return value;
+    }
+}
