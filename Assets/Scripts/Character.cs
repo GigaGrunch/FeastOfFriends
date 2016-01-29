89589e0752a8ef6cfe51c5dd5ef0af9fe7717846
einworB <philip.braun@stud.uni-regensburg.de>
2016-01-29 23:25:37 +0100

    added character script stub

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+    private int strength;
+    private int agility;
+    private int vision;
+
+    private string charName;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public int Strength
+    {
+        get
+        {
+            return strength;
+        }
+
+        set
+        {
+            strength = value;
+        }
+    }
+
+    public int Agility
+    {
+        get
+        {
+            return agility;
+        }
+
+        set
+        {
+            agility = value;
+        }
+    }
+
+    public int Vision
+    {
+        get
+        {
+            return vision;
+        }
+
+        set
+        {
+            vision = value;
+        }
+    }
+
+    public string CharName
+    {
+        get
+        {
+            return charName;
+        }
+
+        set
+        {
+            charName = value;
+        }
+    }
+}
