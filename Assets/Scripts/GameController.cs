12a88a15b27c125c6d6011dc4c021fc83791f752
GigaGrunch <gigagrunch@gmail.com>
2016-01-29 23:44:52 +0100

    created GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    ArrayList<Room> activeRooms;
+    Room selectedRoom;
+    ArrayList<Character> selectedCharacters;
+
+	void removeEmptyRooms()
+    {
+        foreach (Room i in activeRooms)
+        {
+            if (i.characters <= 0)
+            {
+                
+            }
+        }
+    }
+}
