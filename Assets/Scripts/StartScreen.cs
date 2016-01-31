d27a8d3a376279b54f0d6782f1e4f3f8f0efadb8
LenaManschewski <lena.manschewski@stud.uni-regensburg.de>
2016-01-31 14:36:52 +0100

    added Manual in scene

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -2,17 +2,22 @@
 using System.Collections;
 
 public class StartScreen : MonoBehaviour {
-    
 
+    public GameObject panel;
+    bool showingHelp = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         GetComponent<AudioScript>().playIntro();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	    if (showingHelp && Input.GetMouseButton(0))
+        {
+            panel.SetActive(false);
+            showingHelp = false;
+        }
 	}
 
     public void StartNewScreen()
@@ -20,14 +25,20 @@ public class StartScreen : MonoBehaviour {
         Application.LoadLevel("ggj2016");
     }
 
+
+    public void OpenHelp()
+    {
+        panel.SetActive(true);
+        showingHelp = true;
+
+    }
+
     public void QuitGame()
     {
         Application.Quit();
     }
 
-    public void OpenHelp()
-    {
 
-    }
+
 
 }
